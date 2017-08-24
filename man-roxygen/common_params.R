7b43794b5d8457727a8dab8ecf05b942d3583b96
Peter Hickey <peter.hickey@gmail.com>
2017-08-23 21:50:33 -0400

    Minor update to docs of force_block_processing param

--- man-roxygen/common_params.R
+++ man-roxygen/common_params.R
@@ -7,4 +7,6 @@
 #' @param force_block_processing `FALSE` (the default) means that a
 #' seed-aware, optimised method is used (if available). This can be overridden
 #' to use the general block-processing strategy by setting this to `TRUE`
-#' (typically not advised).
+#' (typically not advised). The block-processing strategy loads one or more
+#' (depending on `getOption("DelayedArray.block.size")`) columns (`colFoo()`)
+#' or rows (`rowFoo()`) into memory as an ordinary [base::array].
