ced8c40fbb95669144b99093c0af286353b989fe
Peter Hickey <peter.hickey@gmail.com>
2017-10-03 23:03:38 -0400

    Minor tweak to example

--- man-roxygen/example_dm_HDF5.R
+++ man-roxygen/example_dm_HDF5.R
@@ -1,5 +1,4 @@
 #' @examples
 #' # A DelayedMatrix with a 'HDF5ArraySeed' seed
-#' library(HDF5Array)
-#' dm_HDF5 <- writeHDF5Array(matrix(c(rep(1, 5), 1:5), ncol = 2))
+#' dm_HDF5 <- HDF5Array::writeHDF5Array(matrix(c(rep(1, 5), 1:5), ncol = 2))
 #' class(seed(dm_HDF5))
