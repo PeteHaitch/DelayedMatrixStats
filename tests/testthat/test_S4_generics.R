205036ed1168cc9b7578e44d518a16110ed13f6d
Peter Hickey <peter.hickey@gmail.com>
2017-12-09 18:42:47 -0500

    Minor update to unit test

--- tests/testthat/test_S4_generics.R
+++ tests/testthat/test_S4_generics.R
@@ -6,7 +6,7 @@ test_that("Formals of S4 generic matches matrixStats function", {
   # TODO: Unsure why, but some (implicit?) generics from base are being picked
   #       up, namely `[`, `[[<-`, `[<-`, `$`, and `$<-`
   dms_generics <- dms_generics[dms_generics@package != "base"]
-  dms_generics <- setdiff(dms_generics, "subset_simple_seed_as_seed_class")
+  dms_generics <- setdiff(dms_generics, "subset_by_Nindex")
   lapply(dms_generics, function(dms_generic) {
     ms_fun <- utils::getFromNamespace(dms_generic, "matrixStats")
     expect_identical(formals(dms_generic), formals(ms_fun))
