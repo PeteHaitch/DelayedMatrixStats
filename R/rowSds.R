61832af75f8daa563bb28b711f6fbd32db965efb
Peter Hickey <peter.hickey@gmail.com>
2018-01-25 19:34:02 -0500

    Simplify colSds() and rowSds()

--- R/rowSds.R
+++ R/rowSds.R
@@ -3,32 +3,6 @@
 ###
 
 ### ----------------------------------------------------------------------------
-### Non-exported methods
-###
-
-#' `rowSds()` block-processing internal helper
-#' @inherit matrixStats::rowSds
-.DelayedMatrix_block_rowSds <- function(x, rows = NULL, cols = NULL, ...) {
-  # Check input type
-  stopifnot(is(x, "DelayedMatrix"))
-  stopifnot(!x@is_transposed)
-  DelayedArray:::.get_ans_type(x, must.be.numeric = TRUE)
-
-  # Subset
-  x <- ..subset(x, rows, cols)
-
-  # Compute result
-  val <- rowblock_APPLY(x = x,
-                        APPLY = matrixStats::rowSds,
-                        ...)
-  if (length(val) == 0L) {
-    return(numeric(ncol(x)))
-  }
-  # NOTE: Return value of matrixStats::rowSds() has no names
-  unlist(val, recursive = FALSE, use.names = FALSE)
-}
-
-### ----------------------------------------------------------------------------
 ### Exported methods
 ###
 
@@ -45,37 +19,11 @@
 setMethod("rowSds", "DelayedMatrix",
           function(x, rows = NULL, cols = NULL, force_block_processing = FALSE,
                    ...) {
-            if (!hasMethod("rowSds", class(seed(x))) ||
-                force_block_processing) {
-              message2("Block processing", get_verbose())
-              return(.DelayedMatrix_block_rowSds(x = x,
-                                                 rows = rows,
-                                                 cols = cols, ...))
-            }
-
-            message2("Has seed-aware method", get_verbose())
-            if (DelayedArray:::is_pristine(x)) {
-              message2("Pristine", get_verbose())
-              simple_seed_x <- seed(x)
-            } else {
-              message2("Coercing to seed class", get_verbose())
-              # TODO: do_transpose trick
-              simple_seed_x <- try(from_DelayedArray_to_simple_seed_class(x),
-                                   silent = TRUE)
-              if (is(simple_seed_x, "try-error")) {
-                message2("Unable to coerce to seed class", get_verbose())
-                return(rowSds(x = x,
-                              rows = rows,
-                              cols = cols,
-                              force_block_processing = TRUE,
-                              ...))
-              }
-            }
-
-            rowSds(x = simple_seed_x,
-                   rows = rows,
-                   cols = cols,
-                   ...)
+            sqrt(rowVars(x = x,
+                         rows = rows,
+                         cols = cols,
+                         force_block_processing = force_block_processing,
+                         ...))
           }
 )
 
