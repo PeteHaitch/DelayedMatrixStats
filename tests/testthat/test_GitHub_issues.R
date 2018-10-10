3ba982208413f2b318df6d40613c8bc8bdf95b11
Peter Hickey <peter.hickey@gmail.com>
2018-10-10 16:13:19 +1100

    Finesse skipping test of issue #54 on Windows

--- tests/testthat/test_GitHub_issues.R
+++ tests/testthat/test_GitHub_issues.R
@@ -4,7 +4,6 @@ test_that("Issue 54 is fixed", {
   # NOTE: This is a slow-ish test, so don't test when running tests with small
   #       block size.
   skip_if(getAutoBlockSize() == 8)
-  skip_on_os("windows")
 
   # Small normal matrix
   m1 <- DelayedArray(as.matrix(iris[, 1:4]))
@@ -28,7 +27,20 @@ test_that("Issue 54 is fixed", {
     rowsum(as.matrix(m2), S),
     rowsum(m2, S, force_block_processing = TRUE))
 
+  # dgCMatrix
+  m4 <- DelayedArray(x)
+  S <- sample(1:1000, nrow(m4), replace = TRUE)
+  expect_equal(
+    rowsum(as.matrix(m4), S),
+    rowsum(m4, S, force_block_processing = FALSE))
+  expect_equal(
+    rowsum(as.matrix(m4), S),
+    rowsum(m4, S, force_block_processing = TRUE))
+
   # RleMatrix
+  # NOTE: This test fails on 32-bit Windows because it can't allocate a ~150 Mb
+  #       vector.
+  skip_on_os("windows")
   m3 <- as(m2, "RleMatrix")
   S <- sample(1:1000, nrow(m3), replace = TRUE)
   expect_equal(
@@ -39,14 +51,4 @@ test_that("Issue 54 is fixed", {
     rowsum(as.matrix(m3), S),
     rowsum(m3, S, force_block_processing = TRUE)
   )
-
-  # dgCMatrix
-  m4 <- DelayedArray(x)
-  S <- sample(1:1000, nrow(m4), replace = TRUE)
-  expect_equal(
-    rowsum(as.matrix(m4), S),
-    rowsum(m4, S, force_block_processing = FALSE))
-  expect_equal(
-    rowsum(as.matrix(m4), S),
-    rowsum(m4, S, force_block_processing = TRUE))
 })
